56ebb8803a02f62bbc8be447ee29dad38fd5bbee
Nastya <fridaythethirteens@gmail.com>
2015-11-26 16:23:52 +0300

    full commit

--- common_lib/headers/typedefs.h
+++ common_lib/headers/typedefs.h
@@ -1,5 +1,7 @@
 #pragma once
-# include <vector>
+#include <vector>
+#include <queue>
 
 typedef std::vector<std::vector<bool> > fieldType;
+typedef std::queue<std::vector<bool> > borderQueue;
 typedef long long ll;
