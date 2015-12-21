f45edd7e8a400dad89b0f39dd19f0cdf2e6a7acb
Nastya <fridaythethirteens@gmail.com>
2015-12-21 20:07:06 +0300

    open mp init

---
+++ open_mp/src/main.cpp
@@ -0,0 +1,21 @@
+#include "../common_lib/headers/shell.h"
+#include "field_manager.h"
+#include "../headers/field_manager_commands.h"
+#include <map>
+
+using namespace std;
+
+int main() {
+  map<string, ShellCommand*> commands;
+  FieldManager manager(std::cout);
+  commands.insert(pair<string, ShellCommand* > (string("start"), new StartCommand(cout, manager)));
+  commands.insert(pair<string, ShellCommand* > (string("quit"), new ExitCommand(cout, manager)));
+  commands.insert(pair<string, ShellCommand* > (string("run"), new RunCommand(cout, manager)));
+  commands.insert(pair<string, ShellCommand* > (string("status"), new StatusCommand(cout, manager)));
+  commands.insert(pair<string, ShellCommand* > (string("stop"), new StopCommand(cout, manager)));
+  Shell shell(commands, cout, cin);
+  shell.start();
+  for(std::map<string, ShellCommand*>::iterator it = commands.begin();it != commands.end();it++) {
+    delete(it->second);
+  }
+}
---
+++ open_mp/src/shell_commands/status_command.cpp
@@ -0,0 +1,18 @@
+#include "../../../common_lib/headers/shell_command.h"
+#include "field_manager.h"
+
+class StatusCommand: public ShellCommand {
+  std::ostream& out;
+  FieldManager& manager;
+public:
+  StatusCommand(std::ostream& out, FieldManager& manager):out(out),manager(manager) {
+    ShellCommand::name = "status";
+  }
+  virtual void run(std::string notParsedArgs) {
+    out << "in status!\n";
+    std::vector<std::string> parsedArgs = parseArguments(notParsedArgs);
+    if (checkNumberOfArguments(parsedArgs.size(), 0, out)) {
+      manager.status();
+    }
+  }
+};
